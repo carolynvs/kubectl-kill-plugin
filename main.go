8ea1d4b9001c83c128981c68a4c9c2a965091a4d
Carolyn Van Slyck <carolyn.vanslyck@microsoft.com>
2018-04-05 14:09:14 -0500

    Refactor for clarity

--- main.go
+++ main.go
@@ -15,13 +15,6 @@ import (
 	_ "github.com/golang/glog"
 )
 
-var (
-	c           *kubernetes.Clientset
-	ns          string
-	podName     string
-	gracePeriod int64
-)
-
 func init() {
 	// Initialize glog flags
 	flag.CommandLine.Set("logtostderr", "true")
@@ -34,36 +27,38 @@ func main() {
 		os.Exit(1)
 	}
 
-	podName = os.Args[1]
+	podName := os.Args[1]
 
+	var gracePeriod int64
 	gracePeriodFlag := os.Getenv("KUBECTL_PLUGINS_LOCAL_FLAG_GRACE_PERIOD")
 	if g, err := strconv.ParseInt(gracePeriodFlag, 10, 64); err == nil {
 		gracePeriod = g
 	}
 
-	kill()
+	kill(podName, gracePeriod)
 }
 
-func kill() {
-	loadConfig()
-	removeFinalizers(podName)
-	deletePod(podName)
+func kill(podName string, gracePeriod int64) {
+	client, ns := loadConfig()
+	removeFinalizers(client, ns, podName)
+	deletePod(client, ns, podName, gracePeriod)
 }
 
-func getPod(podName string) *corev1.Pod {
-	pod, err := c.CoreV1().Pods(ns).Get(podName, metav1.GetOptions{})
+func loadConfig() (*kubernetes.Clientset, string) {
+	restConfig, kubeConfig, err := pluginutils.InitClientAndConfig()
 	if err != nil {
 		panic(err)
 	}
-
-	return pod
+	c := kubernetes.NewForConfigOrDie(restConfig)
+	ns, _, _ := kubeConfig.Namespace()
+	return c, ns
 }
 
-func removeFinalizers(podName string) {
-	pod := getPod(podName)
+func removeFinalizers(client *kubernetes.Clientset, ns, podName string) {
+	pod := getPod(client, ns, podName)
 	pod.SetFinalizers([]string{})
 
-	_, err := c.CoreV1().Pods(ns).Update(pod)
+	_, err := client.CoreV1().Pods(ns).Update(pod)
 	if err != nil {
 		panic(err)
 	}
@@ -71,23 +66,23 @@ func removeFinalizers(podName string) {
 	fmt.Printf("removed finalizers from pod %s\n", podName)
 }
 
-func deletePod(podName string) {
+func getPod(client *kubernetes.Clientset, ns, podName string) *corev1.Pod {
+	pod, err := client.CoreV1().Pods(ns).Get(podName, metav1.GetOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	return pod
+}
+
+func deletePod(client *kubernetes.Clientset, ns, podName string, gracePeriod int64) {
 	fmt.Printf("killing %s/%s with a grace period of %ds...\n", ns, podName, gracePeriod)
 
 	opts := &metav1.DeleteOptions{GracePeriodSeconds: &gracePeriod}
-	err := c.CoreV1().Pods(ns).Delete(podName, opts)
+	err := client.CoreV1().Pods(ns).Delete(podName, opts)
 	if err != nil && !errors.IsNotFound(err) {
 		panic(err)
 	}
 
 	fmt.Printf("deleted pod %s\n", podName)
-}
-
-func loadConfig() {
-	restConfig, kubeConfig, err := pluginutils.InitClientAndConfig()
-	if err != nil {
-		panic(err)
-	}
-	c = kubernetes.NewForConfigOrDie(restConfig)
-	ns, _, _ = kubeConfig.Namespace()
 }
